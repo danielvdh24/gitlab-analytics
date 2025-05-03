b9ff71030788de4bf0fb0ae2bfcdcd3a120b183f
danielvdh24 <vandenheuveldaniel2004@gmail.com>
2025-05-03 23:08:13 +0200

    Add web icon and fix dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/gitlab/",
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(mode),
+  },
+  rollupOptions: {
+    treeshake: true,
+  }
 }));
